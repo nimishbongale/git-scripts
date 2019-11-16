be48856a315c93c00df1ead04886765765a2a84e
Nimish Bongale <nimishnb98@gmail.com>
2019-11-16 22:23:32 +0530

    git actions build

--- Linux/cl.sh
+++ Linux/cl.sh
@@ -4,6 +4,7 @@ echo ***** Press Ctrl+C at any point to terminate *****
 echo
 echo Enter the https link of repo: 
 read link
+echo 
 git clone $link
 echo
 echo ===== git ch-br ran successfully! ===== 
