a2ef3c431aeeed72184a7364d4c4f3f30d53533d
Nimish Bongale <nimishnb98@gmail.com>
2019-11-16 21:54:11 +0530

    Changes

---
+++ Linux/docs/INSTRUCTIONS.md
@@ -0,0 +1,17 @@
+# Setup Instructions
+
+Navigate to the setup folder in this subfolder. Find setup.sh.
+
+## Using bash
+
+Run the following commands in your shell terminal:
+
+```bash
+chmod +x setup.sh
+sh setup.sh
+```
+
+---
+
+If everything has been setup correctly, you can try out the list of commands listed [](here).
+If they don't work as expected, you might have to individually give in the commands listed in setup.bat.
--- Linux/docs/setup/setup.sh
+++ Linux/docs/setup/setup.sh
@@ -1,14 +1,14 @@
-git config --global alias.full-send 'Windows/full-send.sh'
+git config --global alias.full-send '../../full-send.sh'
 chmod +x Windows/full-send.sh
-git config --global alias.send 'Windows/send.sh'
+git config --global alias.send '../../send.sh'
 chmod +x Windows/send.sh
-git config --global alias.recv 'Windows/recv.sh'
+git config --global alias.recv '../../recv.sh'
 chmod +x Windows/recv.sh
-git config --global alias.ch-br 'Windows/ch-br.sh'
+git config --global alias.ch-br '../../ch-br.sh'
 chmod +x Windows/ch-br.sh
-git config --global alias.conf 'Windows/conf.sh'
+git config --global alias.conf '../../conf.sh'
 chmod +x Windows/conf.sh
-git config --global alias.br 'Windows/br.sh'
+git config --global alias.br '../../br.sh'
 chmod +x Windows/br.sh
-git config --global alias.cl 'Windows/cl.sh'
+git config --global alias.cl '../../cl.sh'
 chmod +x Windows/cl.sh
