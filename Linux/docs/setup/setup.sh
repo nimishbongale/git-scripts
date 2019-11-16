13269707c75f5c54577868bbeeb14d8b8e854a48
Nimish Bongale <nimishnb98@gmail.com>
2019-11-16 22:30:57 +0530

    git actions check

--- Linux/docs/setup/setup.sh
+++ Linux/docs/setup/setup.sh
@@ -1,14 +1,14 @@
 git config --global alias.full-send '../../full-send.sh'
-chmod +x Windows/full-send.sh
+chmod +x Linux/full-send.sh
 git config --global alias.send '../../send.sh'
-chmod +x Windows/send.sh
+chmod +x Linux/send.sh
 git config --global alias.recv '../../recv.sh'
-chmod +x Windows/recv.sh
+chmod +x Linux/recv.sh
 git config --global alias.ch-br '../../ch-br.sh'
-chmod +x Windows/ch-br.sh
+chmod +x Linux/ch-br.sh
 git config --global alias.conf '../../conf.sh'
-chmod +x Windows/conf.sh
+chmod +x Linux/conf.sh
 git config --global alias.br '../../br.sh'
-chmod +x Windows/br.sh
+chmod +x Linux/br.sh
 git config --global alias.cl '../../cl.sh'
-chmod +x Windows/cl.sh
+chmod +x Linux/cl.sh
