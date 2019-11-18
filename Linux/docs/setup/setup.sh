370605644abb04dcd100e1438cfde20ed6a6cf45
Nimish Bongale <nimishnb98@gmail.com>
2019-11-18 23:03:56 +0530

    Changes

--- Linux/docs/setup/setup.sh
+++ Linux/docs/setup/setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-git config --global alias.full-send '../../full-send.sh'
-chmod +x Linux/full-send.sh
-git config --global alias.send '../../send.sh'
-chmod +x Linux/send.sh
-git config --global alias.recv '../../recv.sh'
-chmod +x Linux/recv.sh
-git config --global alias.ch-br '../../ch-br.sh'
-chmod +x Linux/ch-br.sh
-git config --global alias.conf '../../conf.sh'
-chmod +x Linux/conf.sh
-git config --global alias.br '../../br.sh'
-chmod +x Linux/br.sh
-git config --global alias.cl '../../cl.sh'
-chmod +x Linux/cl.sh
+git config --global alias.full-send '!../../full-send.sh'
+chmod +x ../../full-send.sh
+git config --global alias.send '!../../send.sh'
+chmod +x ../../send.sh
+git config --global alias.recv '!../../recv.sh'
+chmod +x ../../recv.sh
+git config --global alias.ch-br '!../../ch-br.sh'
+chmod +x ../../ch-br.sh
+git config --global alias.conf '!../../conf.sh'
+chmod +x ../../conf.sh
+git config --global alias.br '!../../br.sh'
+chmod +x ../../br.sh
+git config --global alias.cl '!../../cl.sh'
+chmod +x ../../cl.sh
